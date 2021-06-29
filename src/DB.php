2a32d12933e4e1efd983502921eb86237db2f16a
Egor Shitikov <egorshitikov@gmail.com>
2021-06-28 22:21:45 -0700

    Database connection

---
+++ src/DB.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laragento;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/**
+ * Class Database
+ *
+ * @package Corcel
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class Database
+{
+    /**
+     * @var array
+     */
+    protected static $baseParams = [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => 'wp_',
+    ];
+
+    /**
+     * @param array $params
+     * @return \Illuminate\Database\Capsule\Manager
+     */
+    public static function connect(array $params)
+    {
+        $capsule = new Capsule();
+
+        $params = array_merge(static::$baseParams, $params);
+        $capsule->addConnection($params);
+        $capsule->bootEloquent();
+
+        return $capsule;
+    }
+}
